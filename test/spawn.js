4220c07531c086cc60de2883b26621fd89259b6e
Paul Betts <paul@paulbetts.org>
2016-03-21 23:48:35 -0700

    Rig our test to catch this

--- test/spawn.js
+++ test/spawn.js
@@ -6,7 +6,7 @@ const uuidRegex = /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/
 
 describe('The spawnPromise method', function() {
   it('should return a uuid when we call uuid', async function() {
-    // NB: Since we get run via npm run test, we know that npm bins are in our 
+    // NB: Since we get run via npm run test, we know that npm bins are in our
     // PATH.
     let result = await spawnPromise('uuid', []);
     expect(result.match(uuidRegex)).to.be.ok;
@@ -15,9 +15,9 @@ describe('The spawnPromise method', function() {
 
 describe('The spawnDetachedPromise method', function() {
   it('should return a uuid when we call uuid', async function() {
-    // NB: Since we get run via npm run test, we know that npm bins are in our 
+    // NB: Since we get run via npm run test, we know that npm bins are in our
     // PATH.
-    let result = await spawnDetachedPromise('uuid', []);
-    expect(result.match(uuidRegex)).to.be.ok;
+    let result = await spawnDetachedPromise('uuid', ['--help']);
+    expect(result.length > 10).to.be.ok;
   });
 });
